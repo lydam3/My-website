29be954896e1e2e6a5a63d8268de260b67bbb12c
Rick_clmnt <114478056+Rickclmnt@users.noreply.github.com>
2024-04-06 14:57:32 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada:", layout="wide")
+
+# HEADER SECTION
+st.write(":tada:")
+st.subheader("Rickclmnt.com")
+st.title("Coming Soon: stay tuned for a grand reveal.")
+st.write("sorry for the mess! We're currently under construction. But we promise our wesite will be worth the wait.")
+
+
+st.write("\n")
+st.write("\n")
+st.subheader("What We Do:")
+st.write("""
+         We create AI-Powered solutions that make your life easier. \n
+         . AI for Business: Leverage AI to improve operations within your business, keeping you ahead of the competition. \n
+         . Personal Use: Virtual assistants and other applications that aid you in streamlining tasks. \n
+         . Custom AI Apps: Tailored solutions to meet your unique needs. \n
+         . AI Consultation: We provide guidance on how you can optimize and improve your business with AI.""")
+
+st.write("\n")
+st.write("Watch this space for updates!")
+st.write(":wave:")
